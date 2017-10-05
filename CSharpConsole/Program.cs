9bfec64acfb9286c56ae55f7ebdcbdf9acf103b4
brujlo <bruno.brckovic@outlook.com>
2017-10-05 21:28:53 +0200

    Dodali VB.NET projekt

---
+++ CSharpConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿//using System; moramo ga navoditi
+
+namespace CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Zdravo bjac");
+            System.Console.ReadKey(true); //da ne prikazuje slovo koje stisnemo zato TRUE
+        }
+    }
+}
---
+++ VbNetConsole/Module1.vb
@@ -0,0 +1,8 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Zdravo druze")
+        Console.ReadKey(True)
+    End Sub
+
+End Module
